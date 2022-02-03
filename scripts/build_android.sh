d975551e0948843fea0466e8572bdec72fe67521
Marco Herrera-Rendon <mherrerarendon@makemusic.com>
2022-02-02 19:44:54 -0700

    android build scrip cleanup

--- scripts/build_android.sh
+++ scripts/build_android.sh
@@ -1,18 +1,17 @@
-# cargo build --target aarch64-linux-android --release
-# cargo build --target armv7-linux-androideabi --release
-# cargo build --target i686-linux-android --release
-
-# cp tuner-rs/target/i686-linux-android/release/libtuner_rs.so android/app/src/main/jniLibs/x86/libtuner_rs.so
-
 #!/bin/sh
 JNI_LIBS=android/app/src/main/jniLibs
 
+echo "Cleaning previous build..."
 rm -rf $JNI_LIBS
 mkdir $JNI_LIBS
 mkdir $JNI_LIBS/arm64-v8a
 mkdir $JNI_LIBS/armeabi-v7a
 mkdir $JNI_LIBS/x86
 
+echo "Running flutter rust bridge codegen..."
+flutter_rust_bridge_codegen --rust-input tuner-rs/src/api.rs --dart-output lib/api.dart
+
+echo "Building for Android..."
 cd tuner-rs
 cargo build --target aarch64-linux-android --release
 cargo build --target armv7-linux-androideabi --release
@@ -20,6 +19,9 @@ cargo build --target i686-linux-android --release
 cd ..
 
 
+echo "Copying artifacts to $JNI_LIBS..."
 cp tuner-rs/target/aarch64-linux-android/release/libtuner_rs.so $JNI_LIBS/arm64-v8a/libtuner_rs.so
 cp tuner-rs/target/armv7-linux-androideabi/release/libtuner_rs.so $JNI_LIBS/armeabi-v7a/libtuner_rs.so
 cp tuner-rs/target/i686-linux-android/release/libtuner_rs.so $JNI_LIBS/x86/libtuner_rs.so
+
+echo "All done!"
